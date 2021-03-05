1a01f4fb1e7c0015b00da2ded30186e2dbb44741
Aryan Talathi <43784046+aryantalathi@users.noreply.github.com>
2021-03-05 13:22:01 +0530

    Add files via upload

---
+++ where.js
@@ -0,0 +1,7 @@
+myData = [
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[30.0189275,31.499707, 'AUC Avenue، Road، First New Cairo, Cairo Governorate 11835, Egypt'],
+[33.4532165,-112.0719833, 'Phoenix, AZ 85004, USA'],
+[39.3301047,-82.1076022, 'Athens, OH 45701, USA']
+];
